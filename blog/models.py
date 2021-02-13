7bc1ef67377034074342d80baebbda0c7e5eb490
AnKiWoong <ankiwoong@gmail.com>
2021-02-14 07:57:57 +0900

    블로그의 글을 위한 모델 만들기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)  # 제목
+    content = models.TextField()  # 내용
+
+    creted_at = models.DateTimeField()  # 작성일
+    # author : 추후 작성 예정       # 작성자 정보
